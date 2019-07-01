0504ced15d8c3516100012d19350e72d07c37679
Anna Okhrimenko <anna.golinko@gmail.com>
2019-07-01 13:53:25 +0200

    add diff application configs

---
+++ src/main/java/com/golinko/statement/processor/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.golinko.statement.processor.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+}
